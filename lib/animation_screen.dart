db07c82f5d24fe19984318e7d566d216d21c1652
mhmoudsy <mhmoudsyd27@gmail.com>
2023-03-02 04:05:02 +0200

    last updata

---
+++ lib/animation_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class AnimationScreen extends StatefulWidget {
+  const AnimationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AnimationScreen> createState() => _AnimationScreenState();
+}
+
+List<String> items = [];
+final GlobalKey<AnimatedListState> animatedKey = GlobalKey();
+
+class _AnimationScreenState extends State<AnimationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Flutter Animation',
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    addItems();
+                  },
+                  icon: Icon(Icons.add),
+                ),
+                AnimatedList(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index, animation) => buildItem(index),
+                  shrinkWrap: true,
+                  key: animatedKey,
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  buildItem(index) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          color: Colors.deepPurple,
+          child: ListTile(
+            title: Text(
+              items[index],
+              style: TextStyle(color: Colors.white, fontSize: 25.0),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                removeItems(index);
+              },
+              icon: Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      );
+  void removeItems(index) {
+    animatedKey.currentState!.removeItem(index, (context, animation,) {
+      return SizeTransition(
+        sizeFactor: animation,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            color: Colors.red,
+            child: ListTile(
+              title: Text('Delete',style: TextStyle(fontSize: 25.0),),
+
+            ),
+          ),
+        ),
+      );
+    },
+    duration: Duration(milliseconds: 1000),
+    );
+    items.removeAt(index);
+    print(index);
+  }
+
+  void addItems() {
+    // int i =items.length>0?items.length:0;
+    String x = "Item ${items.length + 1}";
+    items.insert(0, x);
+    animatedKey.currentState!.insertItem(0);
+    print(items.length);
+  }
+}
---
+++ lib/components.dart
@@ -0,0 +1,64 @@
+import 'package:courestest/customClipperClass.dart';
+import 'package:courestest/login_screen.dart';
+import 'package:flutter/material.dart';
+
+defaultTextForm({
+  required TextEditingController controller,
+  required FormFieldValidator validator,
+  required TextInputType type,
+  required String label,
+  required IconData prefix,
+  bool isPassword = false,
+  String obscuringCharacter='*',
+  IconData? suffix,
+  VoidCallback? suffixPressed ,
+})=>TextFormField(
+  obscuringCharacter:obscuringCharacter,
+  controller: controller,
+  obscureText:isPassword ,
+  validator: validator,
+  keyboardType: type,
+  decoration: InputDecoration(
+    labelText: label,
+    border: OutlineInputBorder(),
+    prefixIcon: Icon(
+      prefix,
+    ),
+    suffixIcon: IconButton(
+      icon: Icon(
+        suffix,
+      ),
+      onPressed: suffixPressed,
+    ),
+
+
+  ),
+
+);
+
+
+clipPath({
+  required String title,
+})=>ClipPath(
+  clipper: Cliper(),
+  child: Container(
+    color: Colors.deepPurple,
+    height: 220,
+    child:  Center(
+      child: Text(
+        title,
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w500),
+      ),
+    ),
+  ),
+);
+
+navigateAndRemove(context,Widget screen)=>  Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(
+        builder: (context) => screen),
+    result: (route) => false);
+
